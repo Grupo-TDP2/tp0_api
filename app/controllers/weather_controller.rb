de8f1306069d4296a2a5d3a5e54a123f441dda91
Leandro Masello <leandro.masello@wolox.com.ar>
2018-09-02 23:27:51 -0300

    Import cities from owm

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,19 +1,12 @@
 class WeatherController < ApplicationController
   OWM_WEATHER = 'http://api.openweathermap.org/data/2.5/weather'.freeze
-  OWM_FIND = 'http://api.openweathermap.org/data/2.5/find'.freeze
-  COUNTRY_CODE = 'AR'.freeze
   def index
     request = OWM_WEATHER + "?q=#{index_params}&app_id=#{app_id}"
   end
 
   def cities
-    request = HTTParty.get(OWM_FIND + "?q=#{name_param},#{COUNTRY_CODE}&type=like&appid=#{app_id}")
-    response = JSON.parse(request.body)
-    if response['count'] > 0
-      render json: response['list'].map{ |e| { id: e['id'], name: e['name']} }
-    else
-      render json: []
-    end
+    city_name = "#{name_param}%"
+    render json: City.where("name ilike ?", city_name)
   end
 
   private
