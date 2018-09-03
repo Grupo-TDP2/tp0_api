0e47685101bb5f0d9a4ad0b89f2a687df4de93e5
Leandro Masello <leandro.masello@wolox.com.ar>
2018-09-03 00:22:59 -0300

    Add static responses

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,7 +1,10 @@
 class WeatherController < ApplicationController
   OWM_FORECAST = 'http://api.openweathermap.org/data/2.5/forecast'.freeze
+  STATIC_CITY_IDS = ['-1', '-2']
   def index
-    request = OWM_FORECAST + "?q=#{index_params}&app_id=#{app_id}"
+    return render json: static_city if id_of_static_city?
+    request = HTTParty.get(OWM_FORECAST + "?id=#{index_params}&appid=#{app_id}")
+    render json: process_forecast(JSON.parse(request.body))
   end
 
   def cities
@@ -10,6 +13,18 @@ class WeatherController < ApplicationController
   end
 
   private
+
+  def id_of_static_city?
+    STATIC_CITY_IDS.include?(index_params)
+  end
+
+  def static_city
+    index_params == '-1' ? StaticCity.paris_2 : StaticCity.paris_3
+  end
+
+  def process_forecast(owm_forecast)
+    owm_forecast
+  end
 
   def index_params
     params.require(:city_id)
---
+++ app/poros/static_city.rb
@@ -0,0 +1,113 @@
+class StaticCity
+  def self.paris_2
+    {
+      'isDayInCity': true,
+      'day_1': {
+        'midday':{
+          'temperature': 15,
+          'weather': 502
+        },
+        'midnight': {
+          'temperature': 12,
+          'weather': 200
+        }
+      },
+      'day_2': {
+        'midday':{
+          'temperature': -5,
+          'weather': 600
+        },
+        'midnight': {
+          'temperature': 0,
+          'weather': 700
+        }
+      },
+      'day_3': {
+        'midday':{
+          'temperature': 5,
+          'weather': 804
+        },
+        'midnight': {
+          'temperature': 4,
+          'weather': 802
+        }
+      },
+      'day_4': {
+        'midday':{
+          'temperature': 2,
+          'weather': 803
+        },
+        'midnight': {
+          'temperature': 2,
+          'weather': 804
+        }
+      },
+      'day_5': {
+        'midday':{
+          'temperature': 2,
+          'weather': 801
+        },
+        'midnight': {
+          'temperature': 2,
+          'weather': 801
+        }
+      }
+    }
+  end
+
+  def self.paris_3
+    {
+      'isDayInCity': false,
+      'day_1': {
+        'midday':{
+          'temperature': 10,
+          'weather': 801
+        },
+        'midnight': {
+          'temperature': 10,
+          'weather': 801
+        }
+      },
+      'day_2': {
+        'midday':{
+          'temperature': 10,
+          'weather': 801
+        },
+        'midnight': {
+          'temperature': 10,
+          'weather': 800
+        }
+      },
+      'day_3': {
+        'midday':{
+          'temperature': 10,
+          'weather': 800
+        },
+        'midnight': {
+          'temperature': 8,
+          'weather': 800
+        }
+      },
+      'day_4': {
+        'midday':{
+          'temperature': 2,
+          'weather': 300
+        },
+        'midnight': {
+          'temperature': 2,
+          'weather': 300
+        }
+      },
+      'day_5': {
+        'midday':{
+          'temperature': 8,
+          'weather': 300
+        },
+        'midnight': {
+          'temperature': 6,
+          'weather': 200
+        }
+      }
+    }
+  end
+end
